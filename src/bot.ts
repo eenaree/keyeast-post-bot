7d0bdb4a7f8e5581678ee296858a6042e81a6e45
eenaree <narilee91@gmail.com>
2024-02-11 16:07:33 +0900

    refactor application logic with helper functions

--- src/bot.ts
+++ src/bot.ts
@@ -10,24 +10,13 @@ app();
 async function app() {
   const postsFromCrawling = await crawl();
   const issue = await getIssue({ owner: 'eenaree', repo: 'keyeast-post-bot', issue_number: 1 });
-  const newPosts: Post[] = [];
 
   if (issue && issue.data?.body) {
-    const volumenoList = issue.data.body.split('\n');
-    const lastVolumeno = volumenoList[volumenoList.length - 1];
-    postsFromCrawling.forEach((post) => {
-      if (+lastVolumeno < +post.volumeno) {
-        newPosts.unshift(post);
-      }
-    });
+    const lastVolumeno = getLastVolumeno(issue.data.body);
+    const newPosts = getNewPosts(postsFromCrawling, lastVolumeno);
 
     if (newPosts.length > 0) {
-      for (const post of newPosts) {
-        const text = encodeURIComponent(`${post.title}\n${post.link}`);
-        await fetch(
-          `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${text}`
-        );
-      }
+      await sendNotification(newPosts);
 
       const newPostsVolumeno = newPosts.map((post) => post.volumeno).join('\n');
       await updateIssue({
@@ -37,5 +26,34 @@ async function app() {
         body: `${issue.data.body}\n${newPostsVolumeno}`,
       });
     }
+  }
+}
+
+function getLastVolumeno(data: string) {
+  const volumenoList = data.split('\n');
+  return volumenoList[volumenoList.length - 1];
+}
+
+function getNewPosts(posts: Post[], lastVolumeno: string) {
+  const newPosts: Post[] = [];
+  posts.forEach((post) => {
+    if (+lastVolumeno < +post.volumeno) {
+      newPosts.unshift(post);
+    }
+  });
+
+  return newPosts;
+}
+
+async function sendNotification(posts: Post[]) {
+  try {
+    for (const post of posts) {
+      const text = encodeURIComponent(`${post.title}\n${post.link}`);
+      await fetch(
+        `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${text}`
+      );
+    }
+  } catch (error) {
+    throw new Error('failed to send notification');
   }
 }
