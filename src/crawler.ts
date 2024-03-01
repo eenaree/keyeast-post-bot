a4384d50f199b9ca6b0b057e4378cf36d8903c41
eenaree <narilee91@gmail.com>
2024-03-01 16:51:07 +0900

    make scrape function in parallel

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer';
+import puppeteer, { Browser } from 'puppeteer';
 import * as cheerio from 'cheerio';
 
 export type Posts = Map<number, { title: string; link: string }>;
@@ -11,18 +11,34 @@ function getPageUrl(keyword: string) {
 
 export async function crawl() {
   const browser = await puppeteer.launch();
-  const page1 = await browser.newPage();
+
+  try {
+    const [postsPage1, postsPage2] = await Promise.all([
+      scrapePage(browser, '문가영'),
+      scrapePage(browser, '가영'),
+    ]);
+    const mergedPosts = new Map([...postsPage1, ...postsPage2]);
+    return mergedPosts;
+  } catch (error) {
+    throw new Error('failed to scrape the page');
+  } finally {
+    await browser.close();
+  }
+}
+
+async function scrapePage(browser: Browser, keyword: string) {
+  const page = await browser.newPage();
   const posts: Posts = new Map();
 
   try {
-    await page1.goto(getPageUrl('문가영'));
-    await page1.setViewport({ width: 1920, height: 1080 });
+    await page.goto(getPageUrl(keyword));
+    await page.setViewport({ width: 1920, height: 1080 });
   } catch (error) {
-    throw new Error('failed to load the page1');
+    throw new Error(`failed to load the page: keyword ${keyword}`);
   }
 
   try {
-    const content = await page1.content();
+    const content = await page.content();
     const $ = cheerio.load(content);
     const feedElems = $('ul.lst_feed').children();
     feedElems.each((i, elem) => {
@@ -39,45 +55,10 @@ export async function crawl() {
         }
       }
     });
+    return posts;
   } catch (error) {
-    throw new Error('failed to extract posts from the page1');
+    throw new Error(`failed to extract posts from the page: keyword ${keyword}`);
   } finally {
-    await page1.close();
+    await page.close();
   }
-
-  const page2 = await browser.newPage();
-
-  try {
-    await page2.goto(getPageUrl('문가영 광고'));
-    await page2.setViewport({ width: 1920, height: 1080 });
-  } catch (error) {
-    throw new Error('failed to load the page2');
-  }
-
-  try {
-    const content = await page2.content();
-    const $ = cheerio.load(content);
-    const feedElems = $('ul.lst_feed').children();
-    feedElems.each((i, elem) => {
-      if (i > 4) return;
-      for (const attr of elem.attributes) {
-        if (attr.name === 'volumeno') {
-          const volumeno = +attr.value;
-          const title = $(elem).find('.tit_feed').text().split('\t').join('').trim();
-          let href = $(elem).find('a.link_end').attr('href');
-          if (href) {
-            href = href.slice(0, href.indexOf('&searchKeyword'));
-            posts.set(volumeno, { title, link: 'https://post.naver.com' + href });
-          }
-        }
-      }
-    });
-  } catch {
-    throw new Error('failed to extract posts from the page2');
-  } finally {
-    await page2.close();
-    await browser.close();
-  }
-
-  return posts;
 }
